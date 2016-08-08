17a90b37d4cf75060bccd424b72c019595306c32
Cex <cex@cexs-macmini31.home>
2016-08-08 22:20:13 +0100

    Initial Commit

---
+++ showcase-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  showcase-dev
+//
+//  Created by Cex on 08/08/2016.
+//  Copyright © 2016 newmediatechies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
